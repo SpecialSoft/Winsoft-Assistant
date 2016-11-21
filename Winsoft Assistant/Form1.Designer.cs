22e97239625026f0410f69a9bf88ef0cea148750
Arnyminer Z <Arnyminer.Z@gmail.com>
2016-11-21 20:09:32 +0100

    Add project files.

---
+++ Winsoft Assistant/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Winsoft_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Loader = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Loader)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 37);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(559, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "✘";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseHover += new System.EventHandler(this.label1_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(529, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "↓";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // Loader
+            // 
+            this.Loader.Location = new System.Drawing.Point(77, 72);
+            this.Loader.Name = "Loader";
+            this.Loader.Size = new System.Drawing.Size(442, 290);
+            this.Loader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Loader.TabIndex = 3;
+            this.Loader.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(591, 407);
+            this.ControlBox = false;
+            this.Controls.Add(this.Loader);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Loader)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Loader;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Winsoft Assistant/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winsoft_Assistant
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        int animationCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 2;
+            timer.Tick += new EventHandler(animationLoaderTimer);
+            timer.Start();
+        }
+        private void animationLoaderTimer(object sender, EventArgs args)
+        {
+            if (animationCounter > 69)
+                animationCounter = 0;
+            var image = Image.FromFile("C:/Users/Arnyminer Z/Pictures/orangeLoading/frame_" + animationCounter + "_delay-0.02s.gif");
+            Loader.Image = image;
+            animationCounter++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Location = new Point(0, 0);
+            panel1.Width = Width;
+
+            UpdateRecordButtonImage();
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.FromArgb(200, 200, 200);
+        }
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.FromArgb(255, 255, 255);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.FromArgb(200, 200, 200);
+        }
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.FromArgb(255, 255, 255);
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        #region Move Form
+        //Global variables;
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+        
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;  // _dragging is your variable flag
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+        #endregion
+
+        bool mustRecord = true;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            UpdateRecordButtonImage();
+        }
+        private void UpdateRecordButtonImage()
+        {
+            mustRecord = !mustRecord;
+            if(mustRecord)
+                pictureBox1.Image = Image.FromFile("C:/Users/Arnyminer Z/Pictures/orangeLoading/mic.svg");
+            else
+                pictureBox1.Image = Image.FromFile("C:/Users/Arnyminer Z/Pictures/orangeLoading/no_mic.svg");
+        }
+
+
+
+
+        public class Utility
+        {
+            /// <summary>
+            /// Takes the full name of a resource and loads it in to a stream.
+            /// </summary>
+            /// <param name="resourceName">Assuming an embedded resource is a file
+            /// called info.png and is located in a folder called Resources, it
+            /// will be compiled in to the assembly with this fully qualified
+            /// name: Full.Assembly.Name.Resources.info.png. That is the string
+            /// that you should pass to this method.</param>
+            /// <returns></returns>
+            public static Stream GetEmbeddedResourceStream(string resourceName)
+            {
+                return Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            }
+
+            /// <summary>
+            /// Get the list of all emdedded resources in the assembly.
+            /// </summary>
+            /// <returns>An array of fully qualified resource names</returns>
+            public static string[] GetEmbeddedResourceNames()
+            {
+                return Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            }
+        }
+    }
+}
